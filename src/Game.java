893e8dc9c9ea69b06d8b3b535bc4dfaafea0b66e
JamieChopra <72213070+JamieChopra@users.noreply.github.com>
2021-01-25 23:43:16 +0000

    Creating movement between rooms

--- src/Game.java
+++ src/Game.java
@@ -62,12 +62,12 @@ public class Game
         Room cave, banditLair, groggsLair, groggRoom, groggTreasure, temple, discipleRoom, forgerRoom, bossRoom, romanRoom, femtoLair;
 
 
-        // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theater = new Room("in a lecture theater");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
+//        // create the rooms
+//        outside = new Room("outside the main entrance of the university");
+//        theater = new Room("in a lecture theater");
+//        pub = new Room("in the campus pub");
+//        lab = new Room("in a computing lab");
+//        office = new Room("in the computing admin office");
 
         cave = new Room("in a cave dwelling");
         banditLair = new Room("in a bandits cave");
@@ -80,21 +80,53 @@ public class Game
         bossRoom = new Room("standing before Femto: the eternal");
         romanRoom = new Room("in Roman: the head disciples tower");
         femtoLair = new Room("in the room used to summon Femto: the eternal");
-        // initialise room exits
-        outside.setExit("east", theater);
-        outside.setExit("south", lab);
-        outside.setExit("west", pub);
 
-        theater.setExit("west", outside);
+        cave.setExit("north", banditLair);
 
-        pub.setExit("east", outside);
+        banditLair.setExit("west", groggsLair);
+        banditLair.setExit("east", temple);
+        banditLair.setExit("south", cave);
 
-        lab.setExit("north", outside);
-        lab.setExit("east", office);
+        temple.setExit("north", discipleRoom);
+        temple.setExit("west", banditLair);
 
-        office.setExit("west", lab);
+        discipleRoom.setExit("north", forgerRoom);
+        discipleRoom.setExit("east", romanRoom);
+        discipleRoom.setExit("south", temple);
 
-        currentRoom = outside;  // start game outside
+        romanRoom.setExit("north", femtoLair);
+        romanRoom.setExit("south", discipleRoom);
+
+        femtoLair.setExit("south", romanRoom);
+
+        forgerRoom.setExit("south", discipleRoom);
+        forgerRoom.setExit("west", bossRoom);
+
+        bossRoom.setExit("south", forgerRoom);
+
+        groggsLair.setExit("south", banditLair);
+        groggsLair.setExit("north", groggRoom);
+
+        groggRoom.setExit("south", groggsLair);
+        groggRoom.setExit("west", groggTreasure);
+
+        groggTreasure.setExit("south", groggRoom);
+
+//        // initialise room exits
+//        outside.setExit("east", theater);
+//        outside.setExit("south", lab);
+//        outside.setExit("west", pub);
+//
+//        theater.setExit("west", outside);
+//
+//        pub.setExit("east", outside);
+//
+//        lab.setExit("north", outside);
+//        lab.setExit("east", office);
+//
+//        office.setExit("west", lab);
+
+        currentRoom = cave;  // start game outside
     }
 
     /**
